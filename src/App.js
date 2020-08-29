a36664f0448bf88c002bc37290cb33b019a0a86a
Kelvin Brito <britok30@gmail.com>
2020-08-29 15:55:50 -0400

    add routing with reat-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.scss';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
     return (
-        <div className="App">
-            <Login />
-        </div>
+        <Router>
+            <div className="App">
+                <Route path="/home" exact={true} component={Home}></Route>
+                <Route path="/" exact={true} component={Login}></Route>
+            </div>
+        </Router>
     );
 }
 
