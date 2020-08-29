c05a318ea526943d8e1c88cf585fb69e462c175f
Kelvin Brito <britok30@gmail.com>
2020-08-29 15:34:12 -0400

    react reveal install + animation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,36 +1,39 @@
 import React from 'react';
+import Fade from 'react-reveal';
 import './Login.scss';
 
 const Login = () => {
     return (
         <div className="outer-container">
-            <div className="inner-container">
-                <form action="" className="form">
-                    <h2 className="input-header">Login</h2>
-                    <div>
-                        <label htmlFor="username">Email</label>
-                        <input
-                            required
-                            name="username"
-                            type="email"
-                            className="login-input"
-                            placeholder="user@email.com"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input
-                            required
-                            type="password"
-                            className="login-input"
-                            placeholder="password"
-                        />
-                    </div>
-                    <button className="btn btn-primary login-btn">
-                        Sign In
-                    </button>
-                </form>
-            </div>
+            <Fade bottom duration={5000} distance="20px">
+                <div className="inner-container">
+                    <form action="" className="form">
+                        <h2 className="input-header">Login</h2>
+                        <div>
+                            <label htmlFor="username">Email</label>
+                            <input
+                                required
+                                name="username"
+                                type="email"
+                                className="login-input"
+                                placeholder="user@email.com"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                required
+                                type="password"
+                                className="login-input"
+                                placeholder="password"
+                            />
+                        </div>
+                        <button className="btn btn-primary login-btn">
+                            Sign In
+                        </button>
+                    </form>
+                </div>
+            </Fade>
         </div>
     );
 };
