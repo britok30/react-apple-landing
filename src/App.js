b5ff72f56f4e75c78fd11ae095fca4b754bb3806
Kelvin Brito <britok30@gmail.com>
2020-08-29 16:46:30 -0400

    sign up

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.scss';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
+import Register from './components/Register/Register';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
             <div className="App">
                 <Route path="/home" exact={true} component={Home}></Route>
                 <Route path="/" exact={true} component={Login}></Route>
+                <Route path="/signup" exact={true} component={Register}></Route>
             </div>
         </Router>
     );
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import Fade from 'react-reveal';
+import { Link } from 'react-router-dom';
+import '../Login/Login.scss';
 
 const Register = () => {
-    return <div></div>;
+    return (
+        <div className="outer-container">
+            <Fade bottom duration={5000} distance="20px">
+                <div className="inner-container">
+                    <form className="form" autoComplete="off">
+                        <h2 className="input-header">Sign-Up</h2>
+                        <div>
+                            <label htmlFor="username">Full Name</label>
+                            <input
+                                required
+                                name="username"
+                                type="email"
+                                className="login-input"
+                                placeholder="John Doe"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="username">Email</label>
+                            <input
+                                required
+                                name="username"
+                                type="email"
+                                className="login-input"
+                                placeholder="user@email.com"
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <input
+                                required
+                                type="password"
+                                className="login-input"
+                                placeholder="password"
+                            />
+                        </div>
+                        <button className="btn btn-primary login-btn">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
+
+                <div className="sign-up">
+                    Have an account? <Link to="/">Log In</Link>
+                </div>
+            </Fade>
+        </div>
+    );
 };
 
 export default Register;
