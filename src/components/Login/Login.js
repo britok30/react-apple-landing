f60b11b0634b56c86f89343254b98e29c71c35aa
Kelvin Brito <britok30@gmail.com>
2020-08-29 15:11:00 -0400

    login design

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,14 +5,30 @@ const Login = () => {
     return (
         <div className="outer-container">
             <div className="inner-container">
-                <form action="">
+                <form action="" className="form">
+                    <h2 className="input-header">Login</h2>
                     <div>
-                        <input type="text" />
+                        <label htmlFor="username">Email</label>
+                        <input
+                            required
+                            name="username"
+                            type="email"
+                            className="login-input"
+                            placeholder="user@email.com"
+                        />
                     </div>
                     <div>
-                        <input type="text" />
+                        <label htmlFor="password">Password</label>
+                        <input
+                            required
+                            type="password"
+                            className="login-input"
+                            placeholder="password"
+                        />
                     </div>
-                    <button className="btn btn-dark">Login</button>
+                    <button className="btn btn-primary login-btn">
+                        Sign In
+                    </button>
                 </form>
             </div>
         </div>
