440694640a8dc742a4215554d835b87284af100b
Kelvin Brito <britok30@gmail.com>
2020-08-30 17:13:49 -0400

    navbar for home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,39 @@
 import React from 'react';
-import fire from '../../fire';
-import { useHistory } from 'react-router-dom';
-import { useToasts } from 'react-toast-notifications';
+import { Link } from 'react-router-dom';
+import './Home.scss';
+import Fade from 'react-reveal';
+
+// import fire from '../../fire';
+// import { useHistory } from 'react-router-dom';
+// import { useToasts } from 'react-toast-notifications';
 
 const Home = () => {
-    const { addToast } = useToasts();
-    const history = useHistory();
-
-    const handleLogOut = () => {
-        fire.auth().signOut();
-    };
-
     return (
         <div>
-            Home
-            <button
-                onClick={() => {
-                    handleLogOut();
-
-                    addToast(`Logged out`, {
-                        appearance: 'success',
-                        autoDismiss: true,
-                    });
-                    
-                    history.push('/');
-                }}
-            >
-                logout
-            </button>
+            <nav className="navbar">
+                <Link className="navbar-brand" to="/iphone">
+                    <Fade bottom duration={5000} distance="20px">
+                        <ion-icon name="logo-apple"></ion-icon>
+                    </Fade>
+                </Link>
+            </nav>
         </div>
     );
 };
 
 export default Home;
+
+//   <button
+//       onClick={() => {
+//           handleLogOut();
+
+//           addToast(`Logged out`, {
+//               appearance: 'success',
+//               autoDismiss: true,
+//           });
+
+//           history.push('/');
+//       }}
+//   >
+//       logout
+//   </button>;
